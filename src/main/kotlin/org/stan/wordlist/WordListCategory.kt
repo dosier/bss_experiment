b3e816fe5b24784282e529329e8dfea69110f0b2
Stan van der Bend <stanvdbend@gmail.com>
2018-12-09 21:27:11 +0100

    Set HARD_TO_READ_WORDS category text color to BLACK

--- src/main/kotlin/org/stan/wordlist/WordListCategory.kt
+++ src/main/kotlin/org/stan/wordlist/WordListCategory.kt
@@ -16,7 +16,7 @@ import javafx.scene.text.Font
 enum class WordListCategory(val font : Font, val color : Color) {
 
     EASY_TO_READ_WORDS(Font.font("Arial", 30.0), Color.BLACK),
-    HARD_TO_READ_WORDS(Font.font("Monotype Corsiva", 30.0), Color.GREY.deriveColor(0.0, 1.0, 1.0, 0.6));
+    HARD_TO_READ_WORDS(Font.font("Monotype Corsiva", 30.0), Color.BLACK);
 
     fun other() : WordListCategory {
         return if(this == EASY_TO_READ_WORDS)
